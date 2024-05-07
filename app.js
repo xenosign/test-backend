4ac9e2a19d514577566ded9d9d22abf35645a3f9
kdtTetz <xenosign@naver.com>
2024-05-07 22:00:42 +0900

    A

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = 4000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.get((req, res) => {
   console.log(req.param);
