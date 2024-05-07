57c1dfa93d6c563513404b5abdcc4d5e2616e4bc
kdtTetz <xenosign@naver.com>
2024-05-07 22:23:20 +0900

    Update app.js

--- app.js
+++ app.js
@@ -6,8 +6,14 @@ const PORT = 4000;
 app.use(express.json());
 app.use(cors());
 
+let cat = 0;
+let dog = 0;
+
 app.get('/', (req, res) => {
-  console.log(req);
+  if (req.query.favorite === 'dog') dog++;
+  if (req.query.favorite === 'cat') cat++;
+
+  console.log(`강아지 : ${dog} / 고양이 : ${cat}`);
 
   res.status(200).send('투표에 참여해 주셔서 감사합니다!');
 });
